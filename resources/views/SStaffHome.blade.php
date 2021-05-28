1bebe1756f3d20ad0387f636ad70bbd7433c5669
imsahilsharma <sahil411sharma@gmail.com>
2021-05-28 09:33:51 +0530

    commit18

--- resources/views/SStaffHome.blade.php
+++ resources/views/SStaffHome.blade.php
@@ -11,14 +11,13 @@
 						<section class="wrapper style5">
 							<div class="inner">
 
-							<div style="text-align: left;">
+							<div style="text-align: center;">
 							<br>
-							<p>Check Out New Payment Transaction, <a href="/StfViewPay" >Click here!!</a></p>
+							<p>Check Out New Payment Transaction, <a href="/StfViewPay" style="color:green;">Click here!!</a></p>
 							<br>
-							<p>For Checking Students Fee Dues, <a href="/StfViewDue" >Click here!!</a></p>
+							<p>For Checking Students Fee Dues, <a href="/StfViewDue" style="color:green;">Click here!!</a></p>
 							<br>
-							<p>Or else Generate Report with New Data, <a href="/StaffReport" >Click here!!</a></p>
-						
+							<p>Or else Generate Report with New Data, <a href="/StaffReport" style="color:green;">Click here!!</a></p>
 							
 							
 							</div>
